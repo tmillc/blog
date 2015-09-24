886255b88314a7dbed3573407165fcc77af4857a
ch <dodecalogue@gmail.com>
2015-09-24 04:38:00 -0500

    articles controller and associated view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :articles
 
-  # You can have the root of your site routed with "root"
   root 'welcome#index'
 
   # Example of regular route:
