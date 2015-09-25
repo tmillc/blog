aa5f87819dbc34cd4a3014b2312fce780efedd9f
ch <dodecalogue@gmail.com>
2015-09-25 17:41:35 -0500

    edit from all or individual articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,16 @@ class ArticlesController < ApplicationController
   end
 
   def show
-  	@article = Article.find(params[:id])
+  #	@article = Article.find(params[:id])  # moved to a private method
+    @article = find_article
   end
 
   def new
     @article = Article.new
+  end
+  
+  def edit
+    @article = find_article
   end
 
   def create
@@ -25,7 +30,21 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = find_article
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
+    def find_article
+      Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :text)
     end
