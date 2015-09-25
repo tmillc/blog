f8e72bb55be7d7b541b7669f89569f0f8b042f54
ch <dodecalogue@gmail.com>
2015-09-25 01:38:24 -0500

    implemented show action for new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,23 @@
 class ArticlesController < ApplicationController
+  def show
+  	@article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-  	render plain: "Here are the passed parameters: #{params[:article].inspect}"
+#  	render plain: "Here are the passed parameters: #{params[:article].inspect}"
+#  @article = Article.new(params[:article])  # Unauthorized, we must whitelist our params
+  # Additionally, we move the whitelisting to a private method
+  @article = Article.new(article_params)
+
+  @article.save
+  redirect_to @article   # redirects to the show action
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
