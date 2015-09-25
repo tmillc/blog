12c45636eb43ec6cccedb7f8a1bbca62bebd4d2d
ch <dodecalogue@gmail.com>
2015-09-25 15:17:37 -0500

    re-render the new article page if title validation fails

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,16 +8,21 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
 #  	render plain: "Here are the passed parameters: #{params[:article].inspect}"
 #  @article = Article.new(params[:article])  # Unauthorized, we must whitelist our params
-  # Additionally, we move the whitelisting to a private method
-  @article = Article.new(article_params)
-
-  @article.save
-  redirect_to @article   # redirects to the show action
+  # Additionally, we move the whitelisting to a private method, article_params
+    @article = Article.new(article_params)
+  
+  # For validation, we want the user to see that they've failed the min title length
+    if @article.save
+      redirect_to @article   # redirects to the show action
+    else
+      render 'new'  # render instead of redirect_to, because render will pass the @article object back to the 'new' template. redirect_to would tell the browser to issue another request
+    end
   end
 
   private
