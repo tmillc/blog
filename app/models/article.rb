65a8039beae97818aae7479ccb044315d7b70357
ch <dodecalogue@gmail.com>
2015-09-28 16:40:00 -0500

    Generated Comment model and ran migration

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
+  # with the has_many/belongs_to association, we can now ie pull up comments belonging to an article by @article.comments 
+  has_many :comments
   validates :title, presence: true, length: { minimum: 5 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :articles
+  # resources :articles  # now we want nested resources for comments
+  resources :articles do
+    resources :comments
+  end
 
   root 'welcome#index'
 
