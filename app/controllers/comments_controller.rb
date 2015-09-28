1267153b4d9011f0dae45d2179aae7ab610a7df7
ch <dodecalogue@gmail.com>
2015-09-28 17:12:49 -0500

    display comment-maker on Article show, made CommentsController#show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+	def create
+		@article = find_article
+		@comment = @article.comments.create(comment_params)
+		# try articles_path here too
+		redirect_to article_path(@article)
+	end
+
+	private
+	  def comment_params
+	  	params.require(:comment).permit(:commenter, :body)
+	  end
+
+	  def find_article
+        Article.find(params[:article_id])
+      end
+
 end
